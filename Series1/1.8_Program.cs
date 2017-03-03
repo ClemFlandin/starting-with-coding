4ed55afc14d597a19502dec1edc2523cc542f051
ClemFlandin <clemflandin@gmail.com>
2017-03-03 21:36:22 +0100

    Exercices 1.8 & 1.9

---
+++ Series1/1.8_Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_02_09_Algorithmique_Exercice1._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A, B, C;
+            A = "573";
+            B = "19";
+            C = A + B;
+            Console.WriteLine("C est égal à {0} ", C);
+        }
+    }
+}
---
+++ Series1/1.9_Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_02_09_Algorithmique_Exercice1._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "573", B = "19", C;
+
+        }
+    }
+}
