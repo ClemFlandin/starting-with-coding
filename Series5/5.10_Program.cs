3823e7289db0a497cd0e780f85c4fd05ff665ec3
ClemFlandin <clemflandin@gmail.com>
2017-03-03 22:04:03 +0100

    Series5

---
+++ Series5/5.10_Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_02_12_Algorithmique_Exercice5_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int somme = 0;
+            string[] prixStr = Console.ReadLine().Split();
+            for (int i = 0; i < prixStr.Length; i++)
+            {
+                somme += Convert.ToInt32(prixStr[i]);
+            }
+            Console.WriteLine("Vous nous devez : {0}", somme);
+            int temp = 0;
+            int billets10 = somme / 10;
+            temp = somme % 10;
+            int billets5 = temp / 5;
+            temp %= 5;
+            int pieces1 = temp;
+            Console.WriteLine("Veuillez donner {0} billets de 10, {1} billet de 5 et {2} pièces de 1!", billets10, billets5, pieces1);
+        }
+    }
+}
---
+++ Series5/5.11_Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_02_13_Algorithmique_Exercice5_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez rentrer le nombre de chevaux partants!");
+            string nStr = Console.ReadLine();
+            int n = Convert.ToInt32(nStr);
+            Console.WriteLine("Veuillez rentrer le nombre de chevaux joués!");
+            string pStr = Console.ReadLine();
+            int p = Convert.ToInt32(pStr);
+            double X = 0, Y = 0;
+            int factn = 1;
+            for (int i = 1; i < n+1; i++)
+            {
+                factn *= i;
+            }
+            int factp = 1;
+            for (int i = 1; i < p+1 ; i++)
+            {
+                factp *= i;
+            }
+            int factnMoinsp = 1;
+            for (int i = 1; i < (n- p)+1; i++)
+            {
+                factnMoinsp *= i;
+            }
+            X = factn / factnMoinsp;
+            Y = factn / (factp * factnMoinsp);
+            Console.WriteLine("Vous avez une chance sur {0} de  gagner dans en jouant dans l'ordre et une chance sur {1} dans le désordre!", X, Y);
+        }
+    }
+}
