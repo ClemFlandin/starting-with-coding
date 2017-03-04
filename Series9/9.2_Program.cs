fb10a6b89c877434b457a94b1c21ad2fb43633da
ClemFlandin <clemflandin@gmail.com>
2017-03-04 14:24:49 +0100

    Exercice9.2

---
+++ Series9/9.2_Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_02_15_Algorithmique_Exercice9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez rentrer un mot!");
+            string mot = Console.ReadLine();
+            int longueurMot = mot.Length;
+            Console.WriteLine("Votre mot compte : {0} lettres! ", longueurMot);
+        }
+    }
+}
