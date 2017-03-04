6cecf9dfd236c9ad04361d989296e082064f5224
ClemFlandin <clemflandin@gmail.com>
2017-03-04 14:17:22 +0100

    Exercice8.1

---
+++ Series8/8.1_Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_02_15_Algorithmique_Exercice8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tableau = new int[6, 13];
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    tableau[i, j] = 0;
+                    Console.Write("{0} ", tableau[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
