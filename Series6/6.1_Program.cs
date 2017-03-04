7ac8817daf3f6377cf91b5091d0c6c9e8cc06d86
ClemFlandin <clemflandin@gmail.com>
2017-03-04 11:24:10 +0100

    Series6

---
+++ Series6/6.1_Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_02_13_Algorithmique_Exercice6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[10];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = 0;
+                Console.WriteLine(tab[i]);
+            }
+        }
+    }
+}
